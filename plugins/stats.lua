fefa705e667ebc5c1a19a9b352bb15c450e305dc
telecerner <amircerner@gmail.com>
2016-12-21 13:36:26 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'telediamond' then -- Put everything you like :)
+  if matches[1]:lower() == 'cerner' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "telediamond" then -- Put everything you like :)
+    if matches[2] == "cerner" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -140,11 +140,11 @@ end
 
 return {
   patterns = {
-    "^[#!/]([Ss]tats)$",
-    "^[#!/]([Ss]tatslist)$",
-    "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (telediamond)",
-	"^[#!/]([Tt]elediamond)"
+    "^([Ss]tats)$",
+    "^([Ss]tatslist)$",
+    "^([Ss]tats) (group) (%d+)",
+    "^([Ss]tats) (cerner)",
+	"^(cerner)"
     }, 
   run = run
 }
